cafef50dab166d88106d71d739393e16f28da0bf
Ziga <ziga.zajc007@gmail.com>
2022-12-01 21:55:41 +0100

    Added validator to the website

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById('salt').value = Argon2id.randomSalt();
 
-document.getElementById("start").addEventListener('click', () => {
+document.getElementById('start').addEventListener('click', () => {
 	let message = document.getElementById('message').value;
 	let salt = document.getElementById('salt').value;
 	let p = document.getElementById('p').value;
@@ -15,7 +15,22 @@ document.getElementById("start").addEventListener('click', () => {
 	let hashHex = Argon2id.hashDecode(hashEncoded);
 	let timerEnd = calcT(timerStart);
 	document.getElementById('hash').innerHTML = "<b>Hash:</b> " + hashHex + "<br/><b>Hash Encoded:</b> " + hashEncoded;
-	document.getElementById('perf').innerHTML = "Hashing this message took <b>" + timerEnd + "ms</b>.";
+	document.getElementById('perf').innerHTML = "Hashing the message took <b>" + timerEnd + "ms</b>.";
+});
+
+document.getElementById('verify').addEventListener('click', () => {
+	let message = document.getElementById('message2').value;
+	let secret = document.getElementById('secret2').value;
+	let associatedData = document.getElementById('associatedData2').value;
+	let hashEncoded = document.getElementById('hashEncoded').value;
+
+	let timerStart = Date.now();
+	let match = Argon2id.verify(hashEncoded, message, secret, associatedData);
+	let timerEnd = calcT(timerStart);
+	if(match) document.getElementById('validate').innerHTML = "The message <b>DOES</b> match the supplied hash.";
+	if(!match) document.getElementById('validate').innerHTML = "The message does <b>NOT</b> match the supplied hash.";
+
+	document.getElementById('perf').innerHTML = "Verifying the message took <b>" + timerEnd + "ms</b>.";
 });
 
 function calcT(timer){
